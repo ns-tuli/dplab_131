bd1df2b7849b07892ab1f71f1ca5effeef7f6287
Nusrat Siddique <113282774+ns-tuli@users.noreply.github.com>
2024-09-26 21:18:05 +0600

    Update Admin.java

--- lab02_stratergy_pattern/src/Admin.java
+++ lab02_stratergy_pattern/src/Admin.java
@@ -1,1 +1,25 @@
 
+// Admin class
+class Admin {
+    private String name;
+
+    public Admin(String name) {
+        this.name = name;
+    }
+
+    public void manageDriver(Driver driver) {
+        System.out.println(name + " is managing driver: " + driver.getName());
+    }
+
+    public void manageRider(Rider rider) {
+        System.out.println(name + " is managing rider: " + rider.getName());
+    }
+
+    public void viewTripHistory(Trip trip) {
+        System.out.println(name + " is viewing trip history for trip from " + trip.getPickupLocation() +
+                " to " + trip.getDropOffLocation() + ". Distance: " + trip.getDistance() + " km. Fare: " + trip.calculateFare());
+    }
+}
+
+
+
