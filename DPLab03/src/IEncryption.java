4c458248d5868bbc7ac7b2ce3ce24aa1a159b4bd
ns-tuli <nusratsiddiquetuli@gmail.com>
2024-10-10 19:41:57 +0600

    undo

--- DPLab03/src/IEncryption.java
+++
@@ -1,5 +0,0 @@
-public interface IEncryption {
-    String encrypt(String data) ;
-    String decrypt(String encryptedData);
-}
-
--- DPlab04/src/DecoratorForCondiment.java
+++
@@ -1,5 +0,0 @@
-public interface DecoratorForCondiment extends Beverage {
-    String getDescription(); // Description method to be implemented by the concrete decorator
-    double cost();           // Cost method to be implemented by the concrete decorator
-}
-
